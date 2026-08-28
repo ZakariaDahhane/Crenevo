d41d3066584f1f12a69d38e2ce4c6bc086a09b19
Zakaria Dahhane <zakaria.dahhane@oclock.school>
2026-08-28 18:32:53 +0000

    feat: add ismanager middleware

---
+++ app/middlewares/isManager.js
@@ -0,0 +1,13 @@
+import { StatusCodes } from 'http-status-codes';
+
+
+
+export function isManager(req, res, next) {
+    if (req.userRole === 'manager') {
+        return next();
+    }
+
+    return res.status(StatusCodes.FORBIDDEN).render('error', {
+        message: 'Accès refusé. Cette page est réservée aux gestionnaires.',
+    });
+}
