f41b192f878cb12427ff0184a2a2fa46a7525636
Zakaria Dahhane <zakaria.dahhane@oclock.school>
2026-08-31 19:18:11 +0000

    feat: add reservation create view

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import homepageRouter from './app/routes/homepage.route.js';
 import roomRouter from './app/routes/room.route.js';
-import authRouter from './app/routes/auth.route.js'
+import authRouter from './app/routes/auth.route.js';
+import reservationRouter from './app/routes/reservation.route.js';
 import cookieParser from 'cookie-parser';
 import { decodeUserFromToken } from './app/middlewares/decodeUserFromToken.middleware.js';
 import 'dotenv/config';
@@ -24,7 +25,8 @@ app.use(express.static('public'));
 app.use(decodeUserFromToken);
 app.use('/', homepageRouter);
 app.use('/rooms', roomRouter);
-app.use('/', authRouter)
+app.use('/', authRouter);
+app.use('/reservations', reservationRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
